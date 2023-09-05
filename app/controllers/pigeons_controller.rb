7d6c594f355699d6a565ad1bb7d61d504f001ca4
Duarte Martins <duartempmartins@gmail.com>
2023-09-05 11:37:12 +0100

    checkout to pull

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,15 +1,9 @@
 class PigeonsController < ApplicationController
-  before_action :set_pigeon, only: [show]
   def index
     @pigeons = Pigeon.all
   end
 
   def show
-  end
-
-  private
-
-  def set_pigeon
     @pigeon = Pigeon.find(params[:id])
   end
 end
