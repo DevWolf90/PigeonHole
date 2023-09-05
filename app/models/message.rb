d19d4f3d56846c80585d2394dc201f1702e1f606
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-09-05 16:53:17 +0200

    chat implemented turbo working

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   has_one :pigeon
   belongs_to :chat
   belongs_to :sender, class_name: "User", foreign_key: "user_id"
+  # belongs_to :recipient, class_name: "User", foreign_key: "user_id"
 
   after_create_commit -> {
     broadcast_append_to chat, target: "messages", partial: "messages/message", locals: { message: self }
