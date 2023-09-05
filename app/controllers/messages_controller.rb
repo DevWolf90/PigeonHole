3ed428c5495058faf1fb0f5f62a08e51b0b977d0
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-09-05 16:42:43 +0200

    chat function implemented, chat working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,13 @@
 class MessagesController < ApplicationController
 
-  def new
-    @message = Message.new
-  end
-
   def create
     @chat = Chat.find(params[:chat_id])
     @message = Message.new(message_params)
     @message.chat = @chat
-    @message.user_id = current_user.id
+    @message.sender = current_user
     if @message.save
-      redirect_to chat_path(@chat), notice: 'Message sent'
+      flash[:notice] = "Message successfully sent"
+      redirect_to chat_path(@chat)
     else
       render "chats/show", status: :unprocessable_entity
     end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,10 @@
 class Message < ApplicationRecord
   has_one :pigeon
   belongs_to :chat
-  belongs_to :sender, class_name: "User", foreign_key: "sender_id"
+  belongs_to :sender, class_name: "User", foreign_key: "user_id"
 
   after_create_commit -> {
-    broadcast_append_to "chat_#{chat.id}", target: "messages", partial: "messages/message", locals: { message: self }
+    broadcast_append_to chat, target: "messages", partial: "messages/message", locals: { message: self }
   }
 
 end
