f686b7a6344b1f87001724cb7daafd76513b3cc7
Duarte Martins <duartempmartins@gmail.com>
2023-09-06 17:10:38 +0100

    navbar

---
+++ app/javascript/controllers/display_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["searchbar"]
+  connect() {
+  }
+
+  show() {
+    this.searchbarTarget.classList.toggle("d-none")
+    this.searchbarTarget.classList.add("ease")
+  }
+}
