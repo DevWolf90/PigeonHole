768881492d9d4213108709594f5ade9dbf725178
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-09-06 13:13:58 +0200

    started implementing ransack

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -2,16 +2,16 @@ class PigeonsController < ApplicationController
   def index
     @pigeons = Pigeon.all
     @alltags = Gutentag::Tag.names_for_scope(Pigeon)
-    # @q = Pigeon.ransack(params[:q])
-    # @tags = @q.result.includes(:pigeons).tag_name(params[:tag_names])
+    @q = Pigeon.ransack(params[:q])
+
+    if params[:iq] && params[:q][:tag_name_cont].present?
+      @q.tag_names_cont(params[:q][:tag_names_cont])
+    end
+    @pigeons = @q.result(distinct:true)
   end
 
   def show
     @pigeon = Pigeon.find(params[:id])
   end
 
-  # def add_to_favourites
-  #   @pigeon = Pigeon.find(params[:id])
-  #   @pigeon.update(favourite: true)
-  # end
 end
--- app/models/pigeon.rb
+++ app/models/pigeon.rb
@@ -2,4 +2,9 @@ class Pigeon < ApplicationRecord
   has_many :content_categories, through: :labels
   belongs_to :message
   Gutentag::ActiveRecord.call self
+  def self.ransackable_attributes(auth_object = nil)
+    super + %w[tag_names]
+  end
+
+  end
 end
