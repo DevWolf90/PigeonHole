3fbdac9c0d505920a3f0e0957a3b2cf0de106f56
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-09-06 11:52:43 +0200

    pigeons index page w/o filtering

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,6 +1,9 @@
 class PigeonsController < ApplicationController
   def index
     @pigeons = Pigeon.all
+    @alltags = Gutentag::Tag.names_for_scope(Pigeon)
+    # @q = Pigeon.ransack(params[:q])
+    # @tags = @q.result.includes(:pigeons).tag_name(params[:tag_names])
   end
 
   def show
