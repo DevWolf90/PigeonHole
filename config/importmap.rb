5a8634b761bdfe5820628280294a7fe447f839e5
Elias Harder <elias941601@gmail.com>
2023-09-11 16:20:15 +0200

    added scroll to point

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@popperjs/core", to: "popper.js", preload: true
 
 pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.1.0/src/autocomplete.js"
 pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.2.2/dist/js/tom-select.complete.js"
+pin "stimulus-scroll-to", to: "https://ga.jspm.io/npm:stimulus-scroll-to@4.1.0/dist/stimulus-scroll-to.mjs"
