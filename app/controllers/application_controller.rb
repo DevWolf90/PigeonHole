660e4dbfa0105c9fde16f843582bf2fa7f33c9c5
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-09-14 16:29:09 +0200

    fixed problem with nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :nickname])
   end
 end
