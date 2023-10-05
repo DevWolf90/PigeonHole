d17e3cec5a539844a5d86739f32f51198ce843b9
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-10-05 15:21:06 +0200

    refactored home page

--- app/helpers/pigeons_helper.rb
+++ app/helpers/pigeons_helper.rb
@@ -1,36 +1,46 @@
 module PigeonsHelper
   def pigeon_card_variables(pigeon)
     card_class = pigeon.read ? "read" : "unread"
+    particular =
+    if pigeon.favourite? && pigeon.reply_later
+      content_tag(:i, "", class: "fa-solid fa-star heart offset-icon")
+    elsif pigeon.favourite?
+      content_tag(:i, "", class: "fa-solid fa-heart heart offset-icon")
+    elsif pigeon.reply_later?
+      content_tag(:i, "", class: "fa-solid fa-clock reply-icon offset-icon")
+    end
+
+    reply_later_status = pigeon.reply_later ? "reply_later" : "dont-reply-later"
     media_icon =
       case pigeon.media_type
       when "video"
-        content_tag(:i, "", class: "fa-brands fa-youtube pigeon-card-media")
+        content_tag(:i, "", class: "fa-brands fa-youtube fa-stack-1x media-type")
       when "article"
-        content_tag(:i, "", class: "fa-regular fa-newspaper pigeon-card-media")
+        content_tag(:i, "", class: "fa-regular fa-newspaper fa-stack-1x media-type")
       when "book"
-        content_tag(:i, "", class: "fa-solid fa-book pigeon-card-media")
+        content_tag(:i, "", class: "fa-solid fa-book fa-stack-1x media-type")
       when "podcast"
-        content_tag(:i, "", class: "fa-solid fa-microphone pigeon-card-media")
+        content_tag(:i, "", class: "fa-solid fa-microphone fa-stack-1x media-type")
       when "audio", "song", "playlist"
-        content_tag(:i, "", class: "fa-solid fa-music pigeon-card-media")
+        content_tag(:i, "", class: "fa-solid fa-music fa-stack-1x media-type")
       when "movie"
-        content_tag(:i, "", "fa-solid fa-film pigeon-card-media")
+        content_tag(:i, "", "fa-solid fa-film fa-stack-1x media-type")
       else
         ""
       end
 
     pigeon_avatar =
       if pigeon.user.avatar.attached?
-        cl_image_tag pigeon.user.avatar.key, height: 30, width:30, crop: :fill
+        cl_image_tag pigeon.user.avatar.key, height: 30, width:30, class: 'avatar-image'
       else
-        image_tag("https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", height:30, width:30)
+        image_tag("https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000", height:30, width:30, class: 'avatar-image')
       end
 
     pigeon_state = {
       card_class: card_class,
       pigeon_avatar: pigeon_avatar,
       media_icon: media_icon,
-
+      particular: particular
     }
 
     pigeon_state
