f6b67553d2d901a1312da4c361d9051db1b7205a
Yustiniya Khokhlova <yustiniya.i.khokhlova@gmail.com>
2023-10-16 13:55:11 +0200

    categories save properly

--- app/controllers/content_categories_controller.rb
+++ app/controllers/content_categories_controller.rb
@@ -1,7 +1,20 @@
 class ContentCategoriesController < ApplicationController
 
   def index
-    @contentcategories = ContentCategory.where(owner_id: current_user)
+    @content_categories = ContentCategory.where("owner_id = ? OR creator_id = ?", current_user.id, current_user.id)
+    @content_category = ContentCategory.new
+  end
+
+  def create
+    @content_category = ContentCategory.new(content_category_params)
+    @content_category.creator_id = current_user.id
+    @content_category.save
+  end
+
+  private
+
+  def content_category_params
+    params.require(:content_category).permit(:name)
   end
 
 end
